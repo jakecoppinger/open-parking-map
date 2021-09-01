f65f081ccaf7454240cdf41e1a55f683365f403a
Jake Coppinger <jake@jakecoppinger.com>
2021-09-01 22:12:00 +1000

    Add deploy script

---
+++ deploy-production.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Running build..."
+yarn build
+
+echo "Uploading..."
+aws s3 sync dist/ s3://openparkingmap.com/ --delete
+
+# Don't cache on frontend
+aws s3 cp s3://openparkingmap.com/index.html s3://openparkingmap.com/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
+
+echo "Done."
